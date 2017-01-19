f412cc495312cea924b81b9c7d998d2419ed4d09
Raphael Menges <unrealer2@yahoo.de>
2017-01-19 18:47:55 +0100

    First implementation.

--- LSLBroadcaster.py
+++ LSLBroadcaster.py
@@ -1,8 +1,11 @@
 # Import modules
 from pylsl.pylsl import StreamInfo, StreamOutlet, local_clock
 import xdf.xdf as xdf
+import collections
 import time
+import sys
 
+# Python sender example
 '''
 # first create a new stream info (here we set the name to BioSemi,
 # the content-type to EEG, 8 channels, 100 Hz, and float-valued data) The
@@ -38,10 +41,138 @@ while True:
 '''
 
 # Load file
-streams = xdf.load_xdf(r"SampleData.xdf", None, False)
+streams = xdf.load_xdf(r"SampleData.xdf", None, False)[0]
 
-# Created info headers
+###############################################################################
+### CREATE STREAMS WITH INFO HEADERS
+###############################################################################
 
-# Merge all timestamps in a tuple structure pointing from timestamp to data to send
+# List for stream outlets
+outlets = []
 
-# Do a while loop and sleep until next timestamp is reached and data has to be sent
+# Go over streams
+for i in range(len(streams)):
+    
+    # Fetch stream info
+    streamInfo = streams[i]['info']
+    print("--- STREAM FOUND ---")
+    
+    # Extract info
+    name        = streamInfo['name'][0]
+    dataType    = streamInfo['type'][0]
+    cannelCount = streamInfo['channel_count'][0]
+    dataRate    = streamInfo['nominal_srate'][0]
+    dataFormat  = streamInfo['channel_format'][0]
+    identifier  = streamInfo['source_id'][0]
+    
+    # Print resuls
+    print("Name: "          + name)
+    print("Data Type: "     + dataType)
+    print("Channel Count: " + cannelCount)
+    print("Data Rate: "     + dataRate)
+    print("Data Format: "   + dataFormat)
+    print("Identifier: "    + identifier)
+    
+    # Create new info header
+    info = StreamInfo(name, dataType, int(cannelCount), int(dataRate), dataFormat, identifier)
+    
+    # Announce extraction of child values
+    print("Child Values:")
+    
+     # Extract child values
+    children = streamInfo['desc'][0]
+    
+    # Go over values
+    for childKey in children:
+        
+        # Fetch value by key
+        childValue = children[childKey][0]
+        
+        # Is value a sequence ("append_child_value")
+        if(isinstance(childValue, collections.Sequence)):
+            print("   " + childKey + ": " + childValue)
+            info.desc().append_child_value(childKey, childValue)
+            
+        # Is value a mapping ("append_child")
+        elif(isinstance(childValue, collections.Mapping)):
+            print("   " + childKey)
+            child = info.desc().append_child(childKey)
+            
+            # Go over entries within child
+            for innerChildKey in childValue:
+                print("   " + "   " + innerChildKey)
+                child.append_child(innerChildKey)
+                
+                # Now go over even more inner child
+                for innererChild in childValue[innerChildKey]:
+                    
+                    # Now go over innermost child
+                    for innermostChild in innererChild:
+                        print("   " + "   " + "   " + innermostChild + ": " + innererChild[innermostChild][0])
+                        child.append_child_value(innermostChild, innererChild[innermostChild][0])
+                        
+    # Add outlet with information
+    outlets.append(StreamOutlet(info, 32, 360)) # chunk size and buffer for given seconds
+    
+    print("--------------------")
+
+###############################################################################
+### PREPARE STREAMING BY MERGING TIMESTAMPS
+###############################################################################
+
+# List to collect timestamps with meta information
+events = []
+
+# Save timestamps with pointer to outlet and data
+for i in range(len(streams)):
+    
+    # Fetch timestamps
+    timeStamps = streams[i]['time_stamps']
+    
+    # Go through timestamps and store tripple to global list of timestamps
+    for j in range(len(timeStamps)):
+        events.append([timeStamps[j], i, j]) # timeStamp, outletIndex / streamIndex, index of time_series
+        
+# Sort global timestamps after timestamps
+def comparator(x, y):
+    if x[0] < y[0]:
+        return -1 # x smaller than y
+    elif x[0] > y[0]:
+        return 1 # x greater than y
+    else:
+        return 0 # x equal to y
+events.sort(comparator)
+
+###############################################################################
+### SEND DATA
+###############################################################################
+
+# Initial waiting time
+print("--- ABOUT TO START STREAMING ---")
+sys.stdout.flush()
+time.sleep(10)
+
+# Do it while there are events
+print("--- START STREAMING ---")
+eventCount = len(events)
+for i in range(eventCount):
+
+    # Extract data from current event
+    event = events[i]
+    timestamp = event[0]
+    outletIndex = event[1]
+    timeSeriesIndex = event[2]
+    
+    # Get sample data to push
+    sampleData = streams[outletIndex]['time_series'][timeSeriesIndex]
+    
+    # Push sample data to outlet
+    outlets[outletIndex].push_sample(sampleData) # TODO: stamp from hardware
+    
+    # Print timestamp
+    print("Sample at: " + str(timestamp))
+    
+    # Set delta time (TODO: potentially erronous. maybe use some global time measurement)
+    if i < eventCount-1:
+        time.sleep(events[i+1][0]-timestamp)
+print("-----------------------")
